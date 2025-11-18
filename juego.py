3dfd9514733237ca5b5d690e31ec9d4c35ae7971
johan200516 <arayarojasjohan@gmail.com>
2025-11-17 18:49:59 -0600

    v0.4-alpha: Dibujo del mapa en la ventana
    Se hizo la clase "mapa" para generar una matriz, y ahora se dibuja esta matriz con celdas coloreadas en la ventana, tratando de comenzar a hacer el mapa del juego.

--- juego.py
+++ juego.py
@@ -1,17 +1,23 @@
 import tkinter as tk
+from mapa import Mapa  
 
-# Constantes:
-
+# Constantes 
 
 TITULO_JUEGO = "Escapa del Laberinto"
 
-# Dimensiones: 
-# (Por ajustar)
-ancho_ventana = 800
-alto_ventana = 600
+# Tamaño del mapa
+filas_mapa = 20
+columnas_mapa = 30
+tamano_celda = 30 
+
+# Tamaño de la ventana
+ancho_ventana = columnas_mapa * tamano_celda
+alto_ventana = filas_mapa * tamano_celda
 
 # Colores 
-color_fondo = "#222222" 
+color_fondo = "#222222"
+color_muro = "#555555"   
+color_camino = "#BBBBBB" 
 
 class JuegoTK:
     """
@@ -26,13 +32,38 @@ class JuegoTK:
         pos_x = (ancho_pantalla//2) - (ancho_ventana//2)
         pos_y = (alto_pantalla//2) - (alto_ventana//2)
         self.ventana.geometry(f"{ancho_ventana}x{alto_ventana}+{pos_x}+{pos_y}")
-
         self.ventana.resizable(False, False)
-        self.ventana.configure(bg=color_fondo)
+        self.frame_mapa = tk.Frame(self.ventana)
+        self.frame_mapa.pack() 
 
-        # Aquí irá el canvas para el mapa (a desarrollar después)
+        # Logica del mapa
+        self.mapa = Mapa(filas_mapa, columnas_mapa)
 
-        # Aquí va a ir la GUI de energía, puntos, etc.
+        self.dibujar_mapa()
+
+    def dibujar_mapa(self):
+        """
+        Recorre la matriz del mapa y dibuja cada celda en el frame_mapa.
+        """
+        for i in range(self.mapa.filas):
+            for j in range(self.mapa.columnas):
+
+                casilla = self.mapa.matriz[i][j]
+                color_celda = color_camino 
+                
+                if casilla.tipo == "muro":
+                    color_celda = color_muro
+
+                celda_frame = tk.Frame(
+                    self.frame_mapa,
+                    width=tamano_celda,
+                    height=tamano_celda,
+                    bg=color_celda,
+                    borderwidth=1,
+                    relief="solid")
+
+                celda_frame.grid(row=i, column=j)
+                celda_frame.grid_propagate(False)
 
     def iniciar(self):
         """
--- mapa.py
+++ mapa.py
@@ -47,3 +47,32 @@ class Tunel(Casilla):
         self.es_salida = False
         self.es_trampa = False
         self.tipo = "tunel"
+
+class Mapa:
+    """
+    Contiene la matriz del juego y la logica para generarla.
+    """
+    def __init__(self, filas, columnas):
+        self.filas = filas
+        self.columnas = columnas
+        
+        self.matriz = [] 
+        
+        # crea el mapa estatico
+        self.generar_mapa_estatico()
+
+    def generar_mapa_estatico(self):
+        """
+        Crea una matriz básica con con un borde de muro y un centro de camino.
+        """
+        self.matriz = []
+        
+        for i in range(self.filas):
+            fila_actual = [] 
+            for j in range(self.columnas):
+                # Para saber si está en el borde o en el centro:
+                if (i == 0 or i == (self.filas - 1) or j == 0 or j == (self.columnas - 1)):                    
+                    fila_actual.append(Muro())
+                else:
+                    fila_actual.append(Camino())
+            self.matriz.append(fila_actual) #Añadir la fila completa
